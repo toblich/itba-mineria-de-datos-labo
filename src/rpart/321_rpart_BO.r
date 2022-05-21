5c4998e276c78eb184b11920c8fc558548eb50f3
toblich <toblich@users.noreply.github.com>
2022-05-20 21:51:15 -0300

    Bayesian opt

---
+++ src/rpart/321_rpart_BO.r
@@ -0,0 +1,205 @@
+# Optimización Bayesiana de hiperparámetros de  rpart
+
+# limpio la memoria
+rm(list = ls()) # remove all objects
+gc() # garbage collection
+
+library("data.table")
+library("rlist")
+
+library("rpart")
+library("parallel")
+
+# paquetes necesarios para la Bayesian Optimization
+library("DiceKriging")
+library("mlrMBO")
+
+
+# Defino la  Optimización Bayesiana
+
+kBO_iter <- 100 # cantidad de iteraciones de la Optimización Bayesiana
+
+hs <- makeParamSet(
+  makeNumericParam("cp", lower = -1, upper = 0.1),
+  makeIntegerParam("minsplit", lower = 1L, upper = 8000L), # la letra L al final significa ENTERO
+  makeIntegerParam("minbucket", lower = 1L, upper = 2000L),
+  makeIntegerParam("maxdepth", lower = 3L, upper = 20L),
+  forbidden = quote(minbucket > 0.5 * minsplit)
+) # minbucket NO PUEDE ser mayor que la mitad de minsplit
+
+ksemilla_azar <- 100003 # cambiar por la primer semilla
+
+#------------------------------------------------------------------------------
+# graba a un archivo los componentes de lista
+# para el primer registro, escribe antes los títulos
+
+loguear <- function(reg, arch = NA, folder = "./work/", ext = ".txt", verbose = TRUE) {
+  archivo <- arch
+  if (is.na(arch)) archivo <- paste0(folder, substitute(reg), ext)
+
+  if (!file.exists(archivo)) # Escribo los títulos
+    {
+      linea <- paste0(
+        "fecha\t",
+        paste(list.names(reg), collapse = "\t"), "\n"
+      )
+
+      cat(linea, file = archivo)
+    }
+
+  linea <- paste0(
+    format(Sys.time(), "%Y%m%d %H%M%S"), "\t", # la fecha y hora
+    gsub(", ", "\t", toString(reg)), "\n"
+  )
+
+  cat(linea, file = archivo, append = TRUE) # grabo al archivo
+
+  if (verbose) cat(linea) # imprimo por pantalla
+}
+#------------------------------------------------------------------------------
+# particionar agrega una columna llamada fold a un dataset que consiste en una partición estratificada según agrupa
+# particionar( data=dataset, division=c(70,30), agrupa=clase_ternaria, seed=semilla)   crea una partición 70, 30
+# particionar( data=dataset, division=c(1,1,1,1,1), agrupa=clase_ternaria, seed=semilla)   divide el dataset en 5 particiones
+
+particionar <- function(data, division, agrupa = "", campo = "fold", start = 1, seed = NA) {
+  if (!is.na(seed)) set.seed(seed)
+
+  bloque <- unlist(mapply(function(x, y) {
+    rep(y, x)
+  }, division, seq(from = start, length.out = length(division))))
+
+  data[, (campo) := sample(rep(bloque, ceiling(.N / length(bloque))))[1:.N],
+    by = agrupa
+  ]
+}
+#------------------------------------------------------------------------------
+# fold_test  tiene el numero de fold que voy a usar para testear, entreno en el resto de los folds
+# param tiene los hiperparámetros del árbol
+
+ArbolSimple <- function(fold_test, data, param) {
+  # genero el modelo
+  modelo <- rpart("clase_ternaria ~ .",
+    data = data[fold != fold_test, ], # entreno en todo MENOS el fold_test que uso para testing
+    xval = 0,
+    control = param
+  )
+
+  # aplico el modelo a los datos de testing
+  prediccion <- predict(modelo,
+    data[fold == fold_test, ], # aplico el modelo sobre los datos de testing
+    type = "prob"
+  ) # quiero que me devuelva probabilidades
+
+  prob_baja2 <- prediccion[, "BAJA+2"] # esta es la probabilidad de baja
+
+  # calculo la ganancia
+  ganancia_testing <- data[fold == fold_test][
+    prob_baja2 > 1 / 60,
+    sum(ifelse(clase_ternaria == "BAJA+2", 59000, -1000))
+  ]
+
+  return(ganancia_testing) # esta es la ganancia sobre el fold de testing, NO esta normalizada
+}
+#------------------------------------------------------------------------------
+
+ArbolesCrossValidation <- function(data, param, qfolds, pagrupa, semilla) {
+  divi <- rep(1, qfolds) # generalmente  c(1, 1, 1, 1, 1 )  cinco unos
+
+  particionar(data, divi, seed = semilla, agrupa = pagrupa) # particiono en dataset en folds
+
+  ganancias <- mcmapply(ArbolSimple,
+    seq(qfolds), # 1 2 3 4 5
+    MoreArgs = list(data, param),
+    SIMPLIFY = FALSE,
+    mc.cores = 1
+  ) # se puede subir a qfolds si posee Linux o Mac OS
+
+  data[, fold := NULL]
+
+  # devuelvo la primer ganancia y el promedio
+  ganancia_promedio <- mean(unlist(ganancias)) # promedio las ganancias
+  ganancia_promedio_normalizada <- ganancia_promedio * qfolds # aquí normalizo la ganancia
+
+  return(ganancia_promedio_normalizada)
+}
+#------------------------------------------------------------------------------
+# esta función solo puede recibir los parámetros que se están optimizando
+# el resto de los parámetros, lamentablemente se pasan como variables globales
+
+EstimarGanancia <- function(x) {
+  GLOBAL_iteracion <<- GLOBAL_iteracion + 1
+
+  xval_folds <- 5
+  ganancia <- ArbolesCrossValidation(dataset,
+    param = x, # los hiperparámetros del árbol
+    qfolds = xval_folds, # la cantidad de folds
+    pagrupa = "clase_ternaria",
+    semilla = ksemilla_azar
+  )
+
+  # logueo
+  xx <- x
+  xx$xval_folds <- xval_folds
+  xx$ganancia <- ganancia
+  xx$iteracion <- GLOBAL_iteracion
+  loguear(xx, arch = archivo_log)
+
+  return(ganancia)
+}
+#------------------------------------------------------------------------------
+# Aquí empieza el programa
+
+setwd("/Users/tlichtig/Desktop/ITBA/2-mineria-de-datos/labo")
+
+# cargo el dataset
+dataset <- fread("./datasets/paquete_premium_202011.csv") # donde entreno
+
+
+# creo la carpeta donde va el experimento
+# HT  representa  Hyperparameter Tuning
+dir.create("./labo/exp/", showWarnings = FALSE)
+dir.create("./labo/exp/HT3210/", showWarnings = FALSE)
+setwd("/Users/tlichtig/Desktop/ITBA/2-mineria-de-datos/labo/labo/exp/HT3210") # Establezco el Working Directory DEL EXPERIMENTO
+
+
+archivo_log <- "HT321.txt"
+archivo_BO <- "HT321.RDATA"
+
+# leo si ya existe el log, para retomar en caso que se se corte el programa
+GLOBAL_iteracion <- 0
+
+if (file.exists(archivo_log)) {
+  tabla_log <- fread(archivo_log)
+  GLOBAL_iteracion <- nrow(tabla_log)
+}
+
+
+
+# Aquí comienza la configuración de la Bayesian Optimization
+
+funcion_optimizar <- EstimarGanancia
+
+configureMlr(show.learner.output = FALSE)
+
+# configuro la búsqueda bayesiana,  los hiperparámetros que se van a optimizar
+# por favor, no desesperarse por lo complejo
+obj.fun <- makeSingleObjectiveFunction(
+  fn = funcion_optimizar,
+  minimize = FALSE, # estoy Maximizando la ganancia
+  noisy = TRUE,
+  par.set = hs,
+  has.simple.signature = FALSE
+)
+
+ctrl <- makeMBOControl(save.on.disk.at.time = 600, save.file.path = archivo_BO)
+ctrl <- setMBOControlTermination(ctrl, iters = kBO_iter)
+ctrl <- setMBOControlInfill(ctrl, crit = makeMBOInfillCritEI())
+
+surr.km <- makeLearner("regr.km", predict.type = "se", covtype = "matern3_2", control = list(trace = TRUE))
+
+# inicio la optimización bayesiana
+if (!file.exists(archivo_BO)) {
+  run <- mbo(obj.fun, learner = surr.km, control = ctrl)
+} else {
+  run <- mboContinue(archivo_BO)
+} # retomo en caso que ya exista
