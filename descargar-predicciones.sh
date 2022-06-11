6bb7b8bea981f793bd5390256b290abce8ef5b7f
toblich <toblich@users.noreply.github.com>
2022-06-11 20:27:09 -0300

    Script para descargar resultados

--- descargar-predicciones.sh
+++ descargar-predicciones.sh
@@ -4,4 +4,10 @@ DIR=`dirname -- $0`
 
 EXPERIMENT=$1
 
-gsutil -m cp "gs://tobi-data-ecd/exp/$EXPERIMENT/futuro_prediccion_*.csv" "$DIR/exp/$EXPERIMENT"
+if [[ $EXPERIMENT == ZZ* ]]; then
+  gsutil -m cp "gs://tobi-data-ecd/exp/$EXPERIMENT/futuro_prediccion_*.csv" "$DIR/exp/$EXPERIMENT"
+fi
+
+if [[ $EXPERIMENT == HT* ]]; then
+  gsutil -m cp "gs://tobi-data-ecd/exp/$EXPERIMENT/BO_log.txt" "$DIR/exp/$EXPERIMENT"
+fi
