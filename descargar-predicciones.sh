cb4c6b73062eaf7111f0e4cc8369580817ae742d
toblich <toblich@users.noreply.github.com>
2022-06-14 14:57:12 -0300

    Script que descarga más files

--- descargar-predicciones.sh
+++ descargar-predicciones.sh
@@ -5,7 +5,11 @@ DIR=`dirname -- $0`
 EXPERIMENT=$1
 
 if [[ $EXPERIMENT == ZZ* ]]; then
-  gsutil -m cp "gs://tobi-data-ecd/exp/$EXPERIMENT/futuro_prediccion_*.csv" "$DIR/exp/$EXPERIMENT"
+  gsutil -m cp \
+    "gs://tobi-data-ecd/exp/$EXPERIMENT/futuro_prediccion_*.csv" \
+    "gs://tobi-data-ecd/exp/$EXPERIMENT/tb_predicciones.txt" \
+    "gs://tobi-data-ecd/exp/$EXPERIMENT/${EXPERIMENT}_semillerio_*.csv" \
+    "$DIR/exp/$EXPERIMENT" # directorio donde escribir
 fi
 
 if [[ $EXPERIMENT == HT* ]]; then
