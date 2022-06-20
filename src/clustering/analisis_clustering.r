b895cd99850ad163913c19eac16f0cd0e2b14708
toblich <toblich@users.noreply.github.com>
2022-06-20 17:43:23 -0300

    Analisis de clustering - Graficos de medias

--- src/clustering/analisis_clustering.r
+++ src/clustering/analisis_clustering.r
@@ -16,25 +16,25 @@ setorder(d, cluster2, foto_mes)
 
 
 columnas <- colnames(d)
-excluir <- c("foto_mes", "numero_de_cliente")
+excluir <- c("foto_mes", "numero_de_cliente", "pos", "pos2")
 features <- setdiff(columnas, excluir)
 
-cols <- rainbow(7)
 
+feature <- "mpayroll"
+for (feature in features) {
+  writeLines(feature)
+  r <- d[, .(mean = mean(get(feature))), by = .(cluster2, pos2)]
+  y <- "mean"
+  p <- ggplot(r, aes_string(x = "pos2", y = y, group = "cluster2", col = "cluster2")) +
+    labs(title = sprintf("%s media de cada cluster hasta ser BAJA=2", feature), colour = "# Cluster") +
+    geom_point() +
+    geom_line() +
+    geom_dl(mapping = aes(label = cluster2),
+      method = dl.combine(list("last.points", hjust = -2), list("first.points", hjust = 2))) +
+    scale_x_continuous(name = "Meses faltantes hasta baja", breaks = -12:-1) +
+    scale_y_continuous(name = feature) +
+    theme_minimal() +
+    theme(plot.title = element_text(hjust = 0.5, size = 22))
 
-r <- d[ , .(mean=mean(mpayroll)), by=.(cluster2, pos2)]
-print(r)
-
-y <- "mean"
-p <- ggplot(r, aes_string(x = "pos2", y = y, group = "cluster2", col="cluster2")) +
-  geom_point() +
-  geom_line() +
-  geom_dl(mapping = aes(label = cluster2), method = list("last.points", hjust=-2)) +
-  scale_x_continuous(name = "Meses faltantes hasta baja", breaks = -12:-1) +
-  scale_y_continuous(name = "mpayroll") +
-  theme_minimal()
-  # scale_x_discrete(name = "Meses faltantes hasta naja", limits=seq(-12,-1, 1))
-
-
-print(p)
-# p <- (sub_data$pos, sub_data$mean, main = sprintf("Media del mpayroll del cluster %d", cluster), type = "o")
+  print(p)
+}
